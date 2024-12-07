92eb05a229af01066a5a04fb6add221d2fd51194
Jude Ibe Ekeocha <153955257+JudeIbeekeocha@users.noreply.github.com>
2024-12-07 17:07:01 -0500

    Update project files and add new components
    - Modify index.html
    - Remove public/vite.svg
    - Update styles in src/App.css
    - Update src/App.jsx
    - Add new favicon src/assets/aina_favicon.png
    - Add new SVG src/assets/maincontent/send.svg
    - Remove old SVG src/assets/react.svg
    - Add Maincontent component with styles
    - Add Navbar component with styles
    - Update global styles in src/index.css

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    // TODO: link to about page or Render about page
+    // TODO: potentially use an SVG for the question mark
+    <>
+      <header className="header-section">
+        <a href="#" className="about-link">
+          <div className="about-content">?</div>
+        </a>
+      </header>
+    </>
+  );
+}
+
+export default Navbar;
