898f3ac6ffc71477d613900a3788185ad1950ae1
Jude Ibe Ekeocha <153955257+JudeIbeekeocha@users.noreply.github.com>
2024-12-13 14:09:07 -0500

    Took out price value

--- src/components/Maincontent/fetchResponse.js
+++ src/components/Maincontent/fetchResponse.js
@@ -1,25 +1,25 @@
 import { sendResponseToDOM } from "./Maincontent";
 import { getTickerData } from "../Tickertape/Tickertape.js";
-
+// https://aina-backend.onrender.com
 export async function fetchResponse(message) {
   let dataToSend = {
     ticker: message,
   };
-  const dataToFetch = fetch("https://aina-backend.onrender.com/receive-json", {
+  const dataToFetch = await fetch("https://aina-backend.onrender.com/receive-json", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(dataToSend),
   });
-  const [response, tickerInfo] = await Promise.all([dataToFetch, getTickerData([message])]);
+  const response = await dataToFetch;
   const data = await response.json();
 
   let res = await data.received_data;
 
   sendResponseToDOM(res.title, "title");
   sendResponseToDOM(res.publisher, "publisher");
-  sendResponseToDOM(tickerInfo[0]['current-price'], "price");
+  // sendResponseToDOM(res.price, "price");
   sendResponseToDOM(res.body, "body");
   sendResponseToDOM(res.link, "link");
 }
